a4a4badcc3806765a55c4b7afd3cd6401dd4d7d0
JUNYONG <wnsdyd1489@gmail.com>
2023-06-25 13:05:56 +0900

    Modify_Route

--- users/urls.py
+++ users/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = 'users'
 urlpatterns = [
     path('', views.login, name='login'),
-    path('join', views.join, name='join'),
-    path('terms', views.terms, name='terms'),
-    path('terms_of_service', views.terms_of_service, name='terms_of_service'),
-    path('privacy_policy', views.privacy_policy, name='privacy_policy'),
+    path('terms/join/', views.join, name='join'),
+    path('terms/', views.terms, name='terms'),
+    path('terms_of_service/', views.terms_of_service, name='terms_of_service'),
+    path('privacy_policy/', views.privacy_policy, name='privacy_policy'),
 ]
