543c07effe672d7c43d99c2ea8c4029cd00e4246
Sion <pso9809@gmail.com>
2023-06-25 16:17:12 +0900

    delete phone number in EngineerTB, add signup req

--- users/views.py
+++ users/views.py
@@ -1,13 +1,42 @@
+from django.contrib import auth
 from django.shortcuts import render
+from django.http import JsonResponse
+
+from .models import EngineerTB
+
 
 # Create your views here.
-
 def login(request):
     return render(request, 'login.html')
 
 def join(request):
-    return render(request, 'join.html')
+    ## Default page load
+    if request.method == 'GET':
+        return render(request, 'join.html')
+    
+    ## Signup request
+    if request.method == 'POST':
+        if request.POST['inputPassword'] == request.POST['inputPasswordCk']:
+            try:
+                new_user = EngineerTB.objects.create_user(usr_id=request.POST['inputId'],
+                                                        password=request.POST['inputPassword'],
+                                                        name=request.POST['inputName'],
+                                                        # phonenum=,
+                                                        uid=request.POST['inputNumber']
+                                                        )
+            except ValueError:
+                return
 
+## ID duplicate check        
+def do_duplicate_check(request):
+    if request.method == 'GET':
+        usr_id = request.GET.get('inputId')
+        try:
+            _id = EngineerTB.objects.get(usr_id=usr_id)
+            return JsonResponse({'duplicate': 'false'})
+        except:
+            return JsonResponse({'duplicate': 'true'})
+        
 def terms(request):
     return render(request, 'terms.html')
 
