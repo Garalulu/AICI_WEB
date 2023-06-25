3f644717ef7e759cac5048afb30d79b184fc95b5
Sion <pso9809@gmail.com>
2023-06-25 17:01:24 +0900

    change html path and add registration script

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+
 from . import views
+
 
 app_name = 'users'
 urlpatterns = [
-    path('', views.login, name='login'),
+    path('', LoginView.as_view(template_name='users/login.html'), name='login'),
     path('terms/join/', views.join, name='join'),
     path('terms/', views.terms, name='terms'),
     path('terms_of_service/', views.terms_of_service, name='terms_of_service'),
