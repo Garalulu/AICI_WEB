0b0288f7179582861e63a1df6ccde3af4c3e1761
Sion <pso9809@gmail.com>
2023-06-26 14:44:46 +0900

    update duplicate check / login

--- users/urls.py
+++ users/urls.py
@@ -6,12 +6,14 @@ from . import views
 
 app_name = 'users'  
 urlpatterns = [
-    path('', LoginView.as_view(template_name='users/login.html'), name='login'), # 로그인(첫 화면)
+    path('login/', LoginView.as_view(template_name='users/login.html'), name='login'), # 로그인(첫 화면)
+    path('logout/', LogoutView.as_view(next_page='users/login.html'), name='logout'), # logout
+    
     path('terms/join/', views.join, name='join'), # 회원가입
+    path('terms/join/do_duplicate_check/', views.do_duplicate_check),
     path('terms/', views.terms, name='terms'), # 회원가입 이용약관
     path('terms_of_service/', views.terms_of_service, name='terms_of_service'), # 서비스 이용약관
     path('privacy_policy/', views.privacy_policy, name='privacy_policy'), # 개인정보 처리방침
-    path('home/', views.home, name='home'), # 메인 홈
-    path('terms/join/do_duplicate_check/', views.do_duplicate_check)
     
+    path('home/', views.home, name='home'), # 메인 홈
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.contrib.auth import login
+import json
 
 from .models import EngineerTB, UidTB
+from .forms import CustomUserCreationForm
 
 
 # Create your views here.
@@ -11,11 +14,20 @@ def login(request):
 def join(request):
     ## Default page load
     if request.method == 'GET':
-        return render(request, 'users/join.html')
+        form = CustomUserCreationForm()
+        return render(request, 'users/join.html', {'form': form})
     
     ## Signup request
     if request.method == 'POST':
-        if request.POST.get('inputPassword') == request.POST.get('inputPasswordCk'):
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            if user is not None:
+                login(request, user)
+            else:
+                return JsonResponse({'message': 'Login failed'})
+                
+        '''if request.POST.get('inputPassword') == request.POST.get('inputPasswordCk'):
             uid = request.POST.get('inputNumber').strip()
             uid_instance = UidTB.objects.filter(uid=uid) # Change uid to UidTB instance for matching with forein key correctly.
             if uid_instance:
@@ -32,20 +44,19 @@ def join(request):
                     return JsonResponse({'message': 'engineer name does not match'})
             else:
                 return JsonResponse({'message': "Invalid engineer id"})
-        return JsonResponse({'message': 'Registration failed'})
+        return JsonResponse({'message': 'Registration failed'})'''
             
             
 
 ## ID duplicate check        
 def do_duplicate_check(request):
     if request.method == 'GET':
-        usr_id = request.GET.get('inputId')
-        
-        _id = EngineerTB.objects.filter(usr_id=usr_id)
-        if _id:
-            JsonResponse({'duplicate': 'true'})
-        else:
-            return JsonResponse({'duplicate': 'false'})
+        usr_id = request.GET['usr_id']
+        try:
+            _id = EngineerTB.objects.get(usr_id=usr_id)
+        except Exception as e:
+            _id = None
+        return JsonResponse({'duplicate': usr_id if _id is None else 'true'})
             
         
 def terms(request):
