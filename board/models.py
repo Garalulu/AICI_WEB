521db2881c77567af4aec1e20c8bb1953304cc27
junseok123 <rhwnstjr23@naver.com>
2023-06-26 17:30:02 +0900

    board db update

--- board/models.py
+++ board/models.py
@@ -3,7 +3,7 @@ from users.models import EngineerTB
 from django.utils.translation import gettext_lazy as _
 
 class BoardTB(models.Model):
-    brd_id = models.CharField(_("board ID"), unique=True, max_length=30)
+    brd_id = models.AutoField(primary_key=True)  # 자동으로 증가하는 기본 키 필드
     usr_id = models.ForeignKey("users.EngineerTB", on_delete=models.CASCADE)
     brd_title = models.CharField(_("board title"), max_length=200)
     brd_content = models.TextField(_("board content"))
@@ -11,4 +11,4 @@ class BoardTB(models.Model):
     brd_update = models.DateTimeField(auto_now=True)
     
     def __str__(self):
-        return self.brd_id
+        return str(self.brd_id)
