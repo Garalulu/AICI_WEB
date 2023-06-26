661ec6cefcc6034f76ef43ea9843fb7d4914f33d
junseok123 <rhwnstjr23@naver.com>
2023-06-26 16:25:37 +0900

    board update

--- board/models.py
+++ board/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from users.models import EngineerTB
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+class BoardTB(models.Model):
+    brd_id = models.CharField(_("board ID"), unique=True, max_length=30)
+    usr_id = models.ForeignKey("users.EngineerTB", on_delete=models.CASCADE)
+    brd_title = models.CharField(_("board title"), max_length=200)
+    brd_content = models.TextField(_("board content"))
+    brd_create = models.DateTimeField(auto_now_add=True)
+    brd_update = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.brd_id
