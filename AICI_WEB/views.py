0cd000bbec6ed8500a6f07b4715e1b692e66fc59
Sion <pso9809@gmail.com>
2023-06-27 14:17:10 +0900

    relocate home.html and delete index.html

--- AICI_WEB/views.py
+++ AICI_WEB/views.py
@@ -6,4 +6,4 @@ from users.decorators import login_required
 @login_required
 def home(request):
     if request.method == 'GET':
-        return render(request, 'users/home.html')
+        return render(request, 'home.html')
