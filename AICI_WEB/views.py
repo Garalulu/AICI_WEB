c84c94a7778457c589e5fbde577f5fe19879f4e4
JUNYONG <wnsdyd1489@gmail.com>
2023-06-29 00:36:07 +0900

    change_home_voc_design

--- AICI_WEB/views.py
+++ AICI_WEB/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 
 from users.decorators import login_required
 from board.models import BoardTB, UploadFile
+from voc.models import CustomerTB
 
 @login_required
 def home(request):
     if request.method == 'GET':
+        ## 공지사항 부분
         board = BoardTB.objects.all().order_by('-brd_id')
         data = []
         for i in range(3):
@@ -16,5 +18,8 @@ def home(request):
                 'brd_create': board[i].brd_create.strftime('%Y-%m-%d %H:%M:%S') if board[i].brd_create else None,
             }
             data.append(board_data)
+        ## voc부분
+        df = CustomerTB.objects.all()
             
-        return render(request, 'home.html', {'data': data})
+        return render(request, 'home.html', {'data': data,'df':df})
+    
