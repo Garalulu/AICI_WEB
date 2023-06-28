395118ceb9044b8411baf770e0eee9675601af8e
Sion <pso9809@gmail.com>
2023-06-28 14:05:33 +0900

    update construction/views.py

--- construction/views.py
+++ construction/views.py
@@ -3,18 +3,22 @@ from django.http import JsonResponse
 
 from users.decorators import login_required
 from .forms import ConstructionForm
+from .models import ConstructionTB
+
 
 @login_required
 def construction(request):
     if request.method == 'GET':
-        return render(request, 'construction/construction.html')
+        ## load constructions but only for engineer's work center
+        data = ConstructionTB.objects.filter(cent=request.user.uid.cent).order_by('started_at') 
+        return render(request, 'construction/construction.html', {'data': data})
     
 @login_required
 def upload_construction(request):
-    if request.method == 'POST' and request.FILES['file']:
+    if request.method == 'POST' and request.FILES:
         form = ConstructionForm(request.POST, request.FILES)
         if form.is_valid():
             _file = form.save()
-           
+           ## insert AI model here
             return JsonResponse({'message': 'Upload success'})
     return JsonResponse({'message': 'Upload failed'})
