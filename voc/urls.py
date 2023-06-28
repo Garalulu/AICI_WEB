78870f297dec648330c1542407e26c38147a8d65
Sion <pso9809@gmail.com>
2023-06-28 16:30:12 +0900

    update tmcheck

--- voc/urls.py
+++ voc/urls.py
@@ -4,5 +4,4 @@ from voc import views
 app_name = 'voc'
 urlpatterns = [
     path('', views.tmcheck, name='tmcheck'),
-    path('upload/', views.upload_voc, name='voc_upload'),
 ]
--- voc/views.py
+++ voc/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
 
 from users.decorators import login_required
@@ -16,27 +16,15 @@ def tmcheck(request):
         form = VOCForm()
         return render(request, 'voc/tmcheck.html', {'data': data,
                                                     'form': form,})
+    ## get excel file and extract data to db    
     if request.method == 'POST':
         form = VOCForm(request.POST, request.FILES)
         if form.is_valid():
             _file = form.save()
             exceltodb(_file) ## extract data in VOCTB to CustomerTB
-            return JsonResponse({'message': '게시물이 성공적으로 생성되었습니다.'})
+            return redirect('/')
         else:
             return JsonResponse({'message': 'Invalid form data'})
 
 
-## upload_voc
-## get excel file and extract data to db    
-# @login_required
-def upload_voc(request):
-    if request.method == 'POST':
-        form = VOCForm(request.POST, request.FILES)
-        if form.is_valid():
-            _file = form.save()
-            exceltodb(_file) ## extract data in VOCTB to CustomerTB
-            return JsonResponse({'message': '게시물이 성공적으로 생성되었습니다.'})
-        else:
-            return JsonResponse({'message': 'Invalid form data'})
-    return JsonResponse({'message': 'Upload failed'})
     
