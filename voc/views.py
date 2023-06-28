58b025c0a98c9316d4aba81189849d5ea5b78541
JUNYONG <wnsdyd1489@gmail.com>
2023-06-28 13:50:48 +0900

    update_voc

--- voc/views.py
+++ voc/views.py
@@ -33,23 +33,3 @@ def upload_voc(request):
             return JsonResponse({'message': 'Invalid form data'})
     return JsonResponse({'message': 'Upload failed'})
 
-# class ExcelUploadView(View):
-#     def post(self, request):
-#         excelFile = request.FILES['file']
-        
-#         excel = openpyxl.load_workbook(excelFile, data_only=True)
-#         work_sheet = excel.worksheets[0]
-        
-#         all_values = []
-#         for row in work_sheet.rows:
-#             row_value = []
-#             for cell in row:
-#                 row_value.append(cell.value)
-#             all_values.append(row_value)
-            
-#         for row in all_values:
-#             sample_model = SampleModel(Number=row[0], Name=row[1], Item=row[2])
-#             sample_model.save()
-            
-#         response = {'status':1, 'message': '엑셀파일이 업로드 됐습니다.'}
-#         return GttpResponse(json.dumps(response), content_type='application/json')
