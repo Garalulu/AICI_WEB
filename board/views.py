666c280d8a8ee8f35391b5646f3d6b7b648fe1c7
egoing <egoing@gmail.com>
2023-06-28 20:35:35 +0900

    board file upload

--- board/views.py
+++ board/views.py
@@ -19,15 +19,11 @@ def post(request):
             board.save()
 
             if file_form.is_valid():
-                file = request.FILES.get('file')
-                if file:
-                    upload_file = UploadFile(brd_id=board, file=file)
-                    upload_file.save()
-                else:
-                # 파일이 선택되지 않은 경우에 대한 처리
-                    upload_file = UploadFile(brd_id=board)
-                    upload_file.save()
-
+                print("여기")
+                file = file_form.cleaned_data['file']
+                upload_file = UploadFile(brd_id=board, file=file)
+                upload_file.save()
+                
             response_data = {'message': '게시물이 성공적으로 생성되었습니다.'}
             return JsonResponse(response_data)
 
