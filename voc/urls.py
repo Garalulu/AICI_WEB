06fbe1c01ac986724677aa91f9e4d7c4a1f1190b
JUNYONG <wnsdyd1489@gmail.com>
2023-06-28 10:49:40 +0900

    update voc

--- voc/urls.py
+++ voc/urls.py
@@ -4,6 +4,7 @@ from voc import views
 app_name = 'voc'
 urlpatterns = [
     path('', views.tmcheck, name='tmcheck'),
-    path('upload/', views.upload_voc, name='voc_upload'),
-    path('customer_list/', views.customer_list, name='customer_list'),
+    # path('upload/', views.upload_voc, name='voc_upload'),
+    path('upload_voc/', views.upload_voc, name='upload_voc'),
+    # path('excelupload', views.ExcelUploadView.as_view(), name='excel-upload'),
 ]
--- voc/views.py
+++ voc/views.py
@@ -4,12 +4,23 @@ from django.http import JsonResponse
 from users.decorators import login_required
 from .exceltodb import exceltodb
 from .forms import VOCForm
-
+import openpyxl
+from django.views import View
 
 @login_required
 def tmcheck(request):
     if request.method == 'GET':
         return render(request, 'voc/tmcheck.html')
+    if request.method == 'POST' and request.FILES['file']:
+        form = VOCForm(request.POST, request.FILES)
+        if form.is_valid():
+            _file = form.save()
+            exceltodb(_file) ## extract data in VOCTB to CustomerTB
+            return JsonResponse({'message': 'Upload success'})
+        else:
+            return JsonResponse({'message': 'Invalid form data'})
+    return JsonResponse({'message': 'Upload failed'})
+    
     
 @login_required
 def upload_voc(request):
@@ -19,21 +30,27 @@ def upload_voc(request):
             _file = form.save()
             exceltodb(_file) ## extract data in VOCTB to CustomerTB
             return JsonResponse({'message': 'Upload success'})
+        else:
+            return JsonResponse({'message': 'Invalid form data'})
     return JsonResponse({'message': 'Upload failed'})
 
-def customer_list(request):
-    if request.method == 'GET':
-        customers = BoardTB.objects.all()
-        data = []
-
-        for customer in customers:
-            customer_data = {
-                'cust_name': customer.cust_name,
-                'declaration': customer.declaration,
-                'cust_type': customer.cust_type,
-                'cust_num': customer.cus_num,
-                'brd_updateust_ads': customer.brd_updateust_ads,
-            }
-            data.append(customer_data)
-
-        return JsonResponse(data, safe=False)
+# class ExcelUploadView(View):
+#     def post(self, request):
+#         excelFile = request.FILES['file']
+        
+#         excel = openpyxl.load_workbook(excelFile, data_only=True)
+#         work_sheet = excel.worksheets[0]
+        
+#         all_values = []
+#         for row in work_sheet.rows:
+#             row_value = []
+#             for cell in row:
+#                 row_value.append(cell.value)
+#             all_values.append(row_value)
+            
+#         for row in all_values:
+#             sample_model = SampleModel(Number=row[0], Name=row[1], Item=row[2])
+#             sample_model.save()
+            
+#         response = {'status':1, 'message': '엑셀파일이 업로드 됐습니다.'}
+#         return GttpResponse(json.dumps(response), content_type='application/json')
