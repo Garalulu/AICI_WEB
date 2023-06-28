ab57d7fbbe0b2d9a48c9f6f289a58d7d4ce93a1d
egoing <egoing@gmail.com>
2023-06-28 17:06:45 +0900

    board models

--- board/models.py
+++ board/models.py
@@ -16,7 +16,7 @@ class BoardTB(models.Model):
 class UploadFile(models.Model):
     file_id = models.AutoField(primary_key=True)
     brd_id = models.ForeignKey(BoardTB, on_delete=models.CASCADE)
-    file = models.FileField(_("업로드된 파일"), upload_to="board/", null=True, blank=True)
-
+    file = models.FileField(_("uploaded file"), upload_to="board",blank=True, null=True)
+    
     def __str__(self):
         return str(self.file_id)
