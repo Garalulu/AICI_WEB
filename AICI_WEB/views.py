54ead1d1a4d98b9f4b5fefa93b646c44ba834a7b
jhsy0429 <gjhsy0520@gmail.com>
2023-06-30 09:56:12 +0900

    update home board

--- AICI_WEB/views.py
+++ AICI_WEB/views.py
@@ -9,17 +9,8 @@ def home(request):
     if request.method == 'GET':
         ## 공지사항 부분
         board = BoardTB.objects.all().order_by('-brd_id')
-        data = []
-        for i in range(3):
-            board_data = {
-                'brd_id': board[i].brd_id,
-                'brd_title': board[i].brd_title,
-                'usr_name': board[i].usr_id.name,
-                'brd_create': board[i].brd_create.strftime('%Y-%m-%d %H:%M:%S') if board[i].brd_create else None,
-            }
-            data.append(board_data)
         ## voc부분
         df = CustomerTB.objects.all()
             
-        return render(request, 'home.html', {'data': data,'df':df})
+        return render(request, 'home.html', {'data': board,'df':df})
     
